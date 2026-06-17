996d809dde1bd0b2ab4b99c5eccc3d161a7c65bd
guilherme <guilherme.alvarenga021@gmail.com>
2026-06-17 09:02:45 -0300

    feat: tratamento de erro github service

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -3,7 +3,16 @@ const BASE_URL = "https://api.github.com";
 export async function getUserRepos(username) {
   const res = await fetch(`${BASE_URL}/users/${username}/repos`);
 
-  if (!res.ok) throw new Error("Erro ao buscar repos");
+  if (!res.ok) {
+    const error = await res.text();
+
+    console.error("GitHub API Error:", {
+      status: res.status,
+      body: error,
+    });
+
+    throw new Error(`Erro ${res.status}`);
+  }
 
   return res.json();
 }
